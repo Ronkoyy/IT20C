9c044639ee9c618c978915862fb318a9842ebddf
Eron Moquete <eronmoquete4@gmail.com>
2025-11-20 08:15:28 +0800

    renamed to for-loop.js

---
+++ for-loop.js
@@ -0,0 +1,19 @@
+//for loop
+// output = " ";
+// for(i = 1; i <= 5; i++ ){
+//     //console.log(i);
+//     output += i + " ";
+// }
+// console.log(output);
+
+//arrays
+output = " ";
+arr = [1,2,3];
+console.log(arr.length);
+
+for(index = arr.length -1; index >= 0; index-- ){
+    output += arr[index] + " ";
+
+}
+
+console.log(output);
