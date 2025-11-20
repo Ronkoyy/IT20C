44aa56970cf61030de7066fde4fa18a41c8c6751
Eron Moquete <eronmoquete4@gmail.com>
2025-11-20 09:00:07 +0800

    Added array do wile loop

--- do-while-loop.js
+++ do-while-loop.js
@@ -5,10 +5,21 @@
 //     i++;
 // } while (i <= 5);
 
+// output = " ";
+// i = 5;
+// do {
+//     output += i + " ";
+//     i--;
+// } while (i >= 1);
+// console.log(output);
+
+arr = [4,2,3];
+index = 0;
 output = " ";
-i = 1;
-do {
-    output += i;
-    i++;
-} while (i <= 5);
+
+do{
+    output += arr[index] + " ";
+    index++;
+} while(index < arr.length);
+
 console.log(output);
